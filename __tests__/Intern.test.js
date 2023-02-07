1fbf5a788796849c48eb435cf837a10b34c3c887
joshuagoeke <113941639+joshuagoeke@users.noreply.github.com>
2023-02-06 19:45:31 -0600

    using given starter code

--- __tests__/Intern.test.js
+++
@@ -1,7 +0,0 @@
-const { Intern } = require('../lib');
-
-const name = "Mike", email="junior@Zissou.edu", id = 747, school = 'Zissou Academy';
-const intern = new Intern(name, id, email, school);
-
-test('Testing .getRole() method', () => expect(intern.getRole()).toBe('Intern'));
-test('Testing .getSchool() method', () => expect(intern.getSchool()).toBe(school));
