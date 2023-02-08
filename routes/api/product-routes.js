bd97aaf8cc26ac5b8d17c2cf1a7c72f25cb83714
joshuagoeke <113941639+joshuagoeke@users.noreply.github.com>
2023-02-07 23:50:12 -0600

    get a single product works

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -18,15 +18,31 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id',  (req, res) => {
+
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-
+  router.get('/:id', async (req, res) => {
+    try {
+      const productData = await Product.findByPk(req.params.id, {
+        // JOIN with Category, JOIN with Tag using the Product Tag
+        include: [{model: Category}, {model: Tag}],
+      });
+  
+      if (!productData) {
+        res.status(404).json({ message: 'No product found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(productData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
   // attributes: {exclude: [category_id]}, //wrote this with James
   // include:[{ model: Category, 
   // attributes: ['id', 'category_name']}]
-});
+
 
 // create new product
 router.post('/', (req, res) => {
