716887cb45762f087bd82d95e5190a0bdbabe9b9
joshuagoeke <113941639+joshuagoeke@users.noreply.github.com>
2023-02-07 23:21:59 -0600

    got get all and post/create a new product working

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,9 +9,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Category and Tag data
   try{
     const productData = await Product.findAll({
-      attributes: {exclude: [category_id]},
-      include:[{ model: Category, 
-      attributes: ['id', 'category_name']}]
+      include: [{model: Category}, {model: Tag}],
     });
     res.status(200).json(productData);
   }catch(err){
@@ -20,9 +18,14 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id',  (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+
+
+  // attributes: {exclude: [category_id]}, //wrote this with James
+  // include:[{ model: Category, 
+  // attributes: ['id', 'category_name']}]
 });
 
 // create new product
@@ -102,8 +105,20 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: { id: req.params.id }
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product with this id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
